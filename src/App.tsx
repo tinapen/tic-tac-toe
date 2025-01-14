303fd522d2cf98c24af352a09eacb743ccbd31b7
Tinapen <cristinapenaflor20@gmail.com>
2025-01-14 23:20:24 +0800

    tic-tac-toe game

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "./assets/react.svg";
+import "./App.css";
+import { TicTacToe } from "./components/TicTacToe";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Tic-Tac-Toe Game</h1>
+      </header>
+      <main>
+        <TicTacToe />
+      </main>
+    </div>
+  );
+}
+
+export default App;
